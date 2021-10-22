5b4310e290297a3ed69162c80037e627a79c7b3d
Marc O'Brien <marc@marcobrien.co>
2021-10-22 10:17:17 -0700

    Added routes, components and styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from "../components/header.svelte";
+    import Footer from "../components/footer.svelte";
+    </script>
+    
+    <style global lang="postcss">
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+    </style>
+  
+   
+      
+            <Header />
+            <main class="px-6 flex-1">
+                <slot />
+            </main>
+            <Footer />
---
+++ src/routes/about.svelte
@@ -0,0 +1,81 @@
+<article class="px-4 py-24 mx-auto max-w-7xl" itemid="#" itemscope itemtype="http://schema.org/BlogPosting">
+    <div class="w-full mx-auto mb-12 text-left md:w-3/4 lg:w-1/2">
+      <img src="https://spaceholder.cc/900x700" class="object-cover w-full h-64 bg-center rounded-lg" alt="Kutty" />
+      <p class="mt-6 mb-2 text-xs font-semibold tracking-wider uppercase text-primary">Development</p>
+      <h1 class="mb-3 text-3xl font-bold leading-tight text-gray-900 md:text-4xl" itemprop="headline" title="Rise of Tailwind - A Utility First CSS Framework">
+        Rise of Tailwind - A Utility First CSS Framework
+      </h1>
+      <div class="flex mb-6 space-x-2">
+        <a class="text-gray-900 bg-gray-100 badge hover:bg-gray-200" href="#">CSS</a>
+        <a class="text-gray-900 bg-gray-100 badge hover:bg-gray-200" href="#">Tailwind</a>
+        <a class="text-gray-900 bg-gray-100 badge hover:bg-gray-200" href="#">AlpineJS</a>
+      </div>
+      <a class="flex items-center text-gray-700" href="#">
+        <div class="avatar"><img src="/placeholder.jpg" alt="Photo of Praveen Juge" /></div>
+        <div class="ml-2">
+          <p class="text-sm font-semibold text-gray-800">Praveen Juge</p>
+          <p class="text-sm text-gray-500">Jan 02 2021</p>
+        </div>
+      </a>
+    </div>
+  
+    <div class="w-full mx-auto prose md:w-3/4 lg:w-1/2">
+      <p>
+        What if there is an easy way to achieve responsive UI without using any UI kit? Can we create new and fresh designs for every project with a CSS framework? Enter Tailwind CSS, will this be the
+        perfect CSS framework, well let’s find out.
+      </p>
+      <p>Tailwind is a utility-first CSS framework, the keyword being ‘utility’. It is basically a set of classes that you can use in your HTML.</p>
+
+      <p>
+        Therefore, we don’t have to write any custom CSS to get this button. This can be heavily extended to build whole web applications without the need for any other styles apart from a tailwind.
+      </p>
+      <p>...</p>
+    </div>
+  </article>
+  
+<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+      <div class="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500" class="object-cover w-full h-64" alt="" />
+        <div class="p-5 border border-t-0">
+          <p class="mb-3 text-xs font-semibold tracking-wide uppercase">
+            <a href="/" class="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700" aria-label="Category" title="traveling">traveling</a>
+            <span class="text-gray-600">— 28 Dec 2020</span>
+          </p>
+          <a href="/" aria-label="Category" title="Visit the East" class="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700">Visit the East</a>
+          <p class="mb-2 text-gray-700">
+            Sed ut perspiciatis unde omnis iste natus error sit sed quia consequuntur magni voluptatem doloremque.
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+      </div>
+      <div class="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img src="https://images.pexels.com/photos/447592/pexels-photo-447592.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" class="object-cover w-full h-64" alt="" />
+        <div class="p-5 border border-t-0">
+          <p class="mb-3 text-xs font-semibold tracking-wide uppercase">
+            <a href="/" class="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700" aria-label="Category" title="traveling">traveling</a>
+            <span class="text-gray-600">— 28 Dec 2020</span>
+          </p>
+          <a href="/" aria-label="Category" title="Simple is better" class="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700">Simple is better</a>
+          <p class="mb-2 text-gray-700">
+            Sed ut perspiciatis unde omnis iste natus error sit sed quia consequuntur magni voluptatem doloremque.
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+      </div>
+      <div class="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+        <img src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" class="object-cover w-full h-64" alt="" />
+        <div class="p-5 border border-t-0">
+          <p class="mb-3 text-xs font-semibold tracking-wide uppercase">
+            <a href="/" class="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700" aria-label="Category" title="traveling">traveling</a>
+            <span class="text-gray-600">— 28 Dec 2020</span>
+          </p>
+          <a href="/" aria-label="Category" title="Film It!" class="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700">Film It!</a>
+          <p class="mb-2 text-gray-700">
+            Sed ut perspiciatis unde omnis iste natus error sit sed quia consequuntur magni voluptatem doloremque.
+          </p>
+          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
